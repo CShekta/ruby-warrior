88717bd2689918168bf15533da42447d02ca378e
Claire <ClaireS29@gmail.com>
2015-11-06 09:09:49 -0800

    up to level 6

--- beginnerlv5.rb
+++ beginnerlv5.rb
@@ -1,17 +1,19 @@
 class Player
   def play_turn(warrior)
     if warrior.feel.empty?
-      if warrior.health < 20 && warrior.health >= @health.to_i
+      if warrior.health < @health.to_i &&   warrior.health < 7
+        warrior.walk!(:backward)
+      elsif warrior.health < 20 && warrior.health >= @health.to_i
         warrior.rest!
       else
       warrior.walk!
       end
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    elsif warrior.feel.wall?
+      warrior.walk!
     else
-      if warrior.feel.captive?
-        warrior.rescue!
-      else
-        warrior.attack!
-      end
+      warrior.attack!
     end
     @health = warrior.health.to_i
   end
